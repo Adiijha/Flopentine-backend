4523eeea98973ea91e4b5b123974558e9f56a026
Aditya <adityakumarjha.276@gmail.com>
2025-02-08 22:50:42 +0530

    Initilize folders

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieparser from 'cookie-parser';
+
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(cookieparser());
+app.options("*", cors(corsOptions));
+
+import storyRouter from './routes/story.routes.js';
+app.use('/stories', storyRouter);
+
+export {app};
